17f929e4d8359995f296f52f7712ea2a0cf46945
akshxl09 <59478929+akshxl09@users.noreply.github.com>
2021-01-24 17:05:20 +0900

    selenium_crawler ver.3

--- COMTRIS/modules/selenium_crawler/crawl.py
+++ COMTRIS/modules/selenium_crawler/crawl.py
@@ -12,7 +12,7 @@ def selenium_crawler(url, category):
     RP = RegexPreprocessor()
 
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(os.environ['COMTRIS_CHROME_DRIVER_PATH'], chrome_options=chrome_options)
